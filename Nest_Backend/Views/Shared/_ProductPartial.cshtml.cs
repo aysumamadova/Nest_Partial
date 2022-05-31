a9d06560d41d5e24d40c561a914a80f6ddc85e62
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-05-31 10:05:59 +0400

    first

---
+++ Nest_Backend/Views/Shared/_ProductPartial.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nest_Backend.Views.Shared
+{
+    public class _ProductPartial:PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
